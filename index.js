b13bb4dc2e3f1a1cf1d5da79ff5ed123ac30d2f5
Ajna <ajnaanida@gmail.com>
2020-10-02 19:41:14 +0200

    Version 1.0

---
+++ index.js
@@ -0,0 +1,60 @@
+
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.querySelector("#tens");
+    var appendSeconds = document.querySelector("#seconds");
+    var buttonStart = document.querySelector(".button-start");
+    var buttonStop = document.querySelector(".button-stop");
+    var buttonReset = document.querySelector(".button-restart");
+    var Interval ;
+  
+    buttonStart.onclick = function(){
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+    
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
+    }
+
+
+
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+      appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens < 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+      
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    
+    }
+    
+  
+  
